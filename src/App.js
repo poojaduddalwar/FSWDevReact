e91b7e4328d76591f128c0b31a4a02a69e79a74c
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-11-27 10:45:27 +0530

    react component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
-function App() {
+import Navbar from "./Navbar"
+const App = () => {
   return (
     <div className="App">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla vel ipsam quaerat, tempora eveniet non magni rerum voluptatum asperiores sint quia, ratione laborum excepturi esse quas doloribus, similique iste tenetur.
-
-      <h4>I'm heading 4</h4>
-      <h3>I'm heading 3</h3>
+      <Navbar/>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, odit!
     </div>
   );
 }
