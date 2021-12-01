277a16636ef4e4e71fb08a82cc1de6aaf41b32a1
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-12-01 16:49:57 +0530

    useEffect hook

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,41 +1,40 @@
-
-import {useState} from 'react'
+import {useState,useEffect} from 'react';
 
 const Content = () => {
-    const [items,setItems] = useState([])
-    const [value,setValue] = useState('')
+    // console.log('render')
+    const [counter,setCounter] = useState(0)
+    const [content,setContent] = useState([])
 
-    const handleSubmit = () =>{
-        // let arr = items
-        // arr.push(value)
-        // console.log(arr)
-        // setItems(arr)
-        // OR
-        setItems(prev => [...prev,value])
-        setValue('')
+    const APICall = async()=>{
+        const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+        const data = await res.json()
+        setContent(data)
     }
 
-    const handleInput = (e) => {
-        setValue(e.target.value)
-        // console.log('value change')
-    }
+    useEffect(()=>{
+        APICall()
+    },[])
 
-    const handleDelete = (item)=>{
-        console.log(item)
-        setItems(prev => prev.filter(i => i !== item))
-    }
+    useEffect(()=>{
+        console.log('counter changed')
+    },[counter])
 
-    // console.log(value)
-    return (  
-        <>
-            <h2>Todo</h2>
-                <input value={value} onChange={handleInput} type="text" />
-                <button onClick={handleSubmit} type="submit">Add</button>
-            <ul>
-                {items.map(item => <li>{item} <button onClick={()=>{handleDelete(item)}}>Delete</button></li>)}
-            </ul>
-        </>
+    return (
+        <div>
+        <h2>{counter}</h2>
+        <button onClick={()=>{setCounter(prev => prev+1)}}>Click Me!</button>
+        <ul>
+            {content.map(item => <li style={{margin:"3px",border:"3px solid black"}}>{item.title}</li>)}
+        </ul>
+        </div>
     );
 }
 
-export default Content; 
+export default Content;
+
+//useEffect is a hook that allows to hook into the life cycle methods .
+//it accepts two parameters : 1. function which is going to run 
+//                            2. dependencies 
+
+//u can never make useEffect async
+//u can have multiple useEffect
