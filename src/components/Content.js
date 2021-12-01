0b04b1371850e38fdd2a8a88b641457944b8807e
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-12-01 13:54:42 +0530

    todo list - using useState - 1

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,64 +1,41 @@
-//FUNCTION COMPONENT 
-
-import ListItem from "./ListItem";
 
 import {useState} from 'react'
 
 const Content = () => {
+    const [items,setItems] = useState([])
+    const [value,setValue] = useState('')
 
-    // let counter = 0
-    // let and var will not work because react will not register the CHANGE
-    //react only registers a state change when you change it using this mysteryFunction.
-    const [counter,setCounter] = useState(1) 
-    // console.log(counter)
-    // console.log(mysteryFunction)
+    const handleSubmit = () =>{
+        // let arr = items
+        // arr.push(value)
+        // console.log(arr)
+        // setItems(arr)
+        // OR
+        setItems(prev => [...prev,value])
+        setValue('')
+    }
 
+    const handleInput = (e) => {
+        setValue(e.target.value)
+        // console.log('value change')
+    }
+
+    const handleDelete = (item)=>{
+        console.log(item)
+        setItems(prev => prev.filter(i => i !== item))
+    }
+
+    // console.log(value)
     return (  
-        <div className="container">
-            <h1>{counter}</h1>
-            <button onClick={()=>{
-                // console.log('button clicked')
-                // console.log(counter)
-                // counter = counter + 1
-                setCounter(previousValue => {
-                    // console.log(previousValue)
-                    return previousValue+1;
-                })
-                console.log(counter)
-            }}>ClickMe</button>
-        </div>
+        <>
+            <h2>Todo</h2>
+                <input value={value} onChange={handleInput} type="text" />
+                <button onClick={handleSubmit} type="submit">Add</button>
+            <ul>
+                {items.map(item => <li>{item} <button onClick={()=>{handleDelete(item)}}>Delete</button></li>)}
+            </ul>
+        </>
     );
 }
 
 export default Content; 
-// useState HOOK ALLOWS US TO HOOK INTO THE FUNCTIONALITY OF ACTUAL STATE
-// IN CLASS COMPONENT WE HAD THE ACCESS TO THE STATE VARIABLE WHILE IN FUNCTION COMPONENT WE DON'T SO WE HAD TO USE THIS useState HOOK
-//Hooks don’t work inside classes. But you can use them instead of writing classes.
-
-
-//CLASS COMPONENT 
-// import React from 'react';
-
-// class Content extends React.Component {
-//     constructor() {
-//         super();
-//         //HERE STATE IS AN OBJECT IN WHICH WE CAN CREATE MULTIPLE KEY VALUE PAIR AND CAN UPDATE THEM AND REACT WILL REGISTER EACH CHANGE
-//         this.state = {
-//             count: 0
-//         };
-//     }
-//     render() {
-//         return (
-//             <div className="container">
-//                 {/* <h1>{counter}</h1>
-//                 <button onClick={() => {
-//                     console.log('button clicked')
-//                     console.log(counter)
-//                     counter = counter + 1
-//                 }}>ClickMe</button> */}
-//                 loremipnsansjcnjcnsdjcndncdjcsnjcndj 
-//             </div>
-//         )
-//     }
-// }
-// export default Content;
